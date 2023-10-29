657fab60b62e3cc816041bf97af90a1cdc333f10
amisu <hamisurahman@gmail.com>
2023-10-29 17:28:09 +0000

    Add assets

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
     <div className="App">
-      <div className="gradient_bg">
+      <div className="gradient__bg">
         <Navbar />
         <Header />
       </div>
