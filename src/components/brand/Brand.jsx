4de7486fe5a506b61de9264c1d21ca2bd08460e0
amisu <hamisurahman@gmail.com>
2023-11-01 13:40:11 +0000

    Style brands

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,18 @@
+import './branch.css'
+import {google, slack, atlassian, dropbox, shopify} from './imports.js'
 
 function Brand() {
   return (
-    <div>brand</div>
-  )
+    <div className="gpt3__brand section__padding">
+      <div><img src={google} alt="goole" /></div>
+      <div><img src={slack} alt="slack" /></div>
+      <div><img src={atlassian} alt="atlassian" /></div>
+      <div><img src={dropbox} alt="dropbox" /></div>
+      <div><img src={shopify} alt="shopify" /></div>
+
+    </div>
+    
+    )
 }
 
 export default Brand
