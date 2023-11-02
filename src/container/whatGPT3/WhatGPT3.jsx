a59d206976e9c4d7d0d578d5fc7073196430e510
amisu <hamisurahman@gmail.com>
2023-11-02 06:02:49 +0000

    Style feature components

--- src/container/whatGPT3/WhatGPT3.jsx
+++ src/container/whatGPT3/WhatGPT3.jsx
@@ -1,8 +1,24 @@
+import Feature from '../features/Feature'
 import './whatGPT3.css'
 
 function WhatGPT3() {
   return (
-    <div>WhatGPT3</div>
+    <div className="gpt3__whatgpt3 section__padding">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature title='what is GPT3' text='is th eag gakg agkajg agkajgakjgkagjk agkjagangihgie8yi;ght .agkl' />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient-text gradient__text">
+          The possiblity are beyond your imagination
+        </h1>
+        <p className='gradient__text'>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature title='Chatbots' text='is th eag gakg agkajg agkajgakjgkagjk agkjagangihgie8yi;ght .agkl' />
+        <Feature title='Knowlegebase' text='akjgkajtin the mean  wiiould to ltekgjkjagti' />
+        <Feature  title='Education ' text='gakgjagkjakjgkajgkjgityiuub qttjeo; thjhfh' />
+      </div>
+  </div>
   )
 }
 
