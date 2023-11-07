5ab259aad52cbd232a1928ee9fc656d9072dbd1e
amisu <hamisurahman@gmail.com>
2023-11-07 17:38:57 +0000

    Footer responsiveness

--- src/container/footer/Footer.jsx
+++ src/container/footer/Footer.jsx
@@ -1,8 +1,48 @@
-import './footer.css'
+import "./footer.css";
+import gpt3Logo from "../../assets/logo.svg";
+
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">
+          Do you want to step into the future before others
+        </h1>
+      </div>
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={gpt3Logo} alt="logo" />
+          <p>Crecetewoord k12 182 DK alknjkcb, all</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Get in touch</h4>
+          <p>creachwood k2 1^2 Dk Alkskjgs, all</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+      </div>
+      <div className="gpt3__footer-copyright">
+        <p> C 2020 GPT-3. All Rights reserved</p>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
